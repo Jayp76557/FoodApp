0acb1befc49e55d75d7a144ba7111aebf690abed
Jay Patel <jayp76557@gmail.com>
2024-10-24 16:24:28 -0400

    some message

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`flex-1 justify-center items-center bg-black`}>
+      <Text style={tw` text-white text-3xl font-bold`}>Open up App.js to start working on your 🤩😒</Text>
+      <StatusBar style="light" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
